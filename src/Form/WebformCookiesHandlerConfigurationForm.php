da0e1bee7550e3530f2365d0e4bbe081c9668836
Zachary Oines <oines006@umn.edu>
2019-05-16 12:55:40 -0500

    cleaned up admin user interface.

--- src/Form/WebformCookiesHandlerConfigurationForm.php
+++ src/Form/WebformCookiesHandlerConfigurationForm.php
@@ -75,7 +75,21 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
     if (is_string($default_forwarding_url)) { 
       $default_forwarding_url  =  $default_forwarding_url ;
     } else { 
-      $default_forwarding_url  = 'Enter url here...';
+      $default_forwarding_url  = 'Enter fully qualified url here: https://example.com/post';
+    }
+
+    $default_on_new_submissions_forwarding_checkbox = $config->get('default_on_new_submissions_forwarding_checkbox');
+    if (is_numeric($default_on_new_submissions_forwarding_checkbox)) { 
+      $default_on_new_submissions_forwarding_checkbox =  $default_on_new_submissions_forwarding_checkbox;
+    } else { 
+      $default_on_new_submissions_forwarding_checkbox = 0;
+    }
+
+    $default_on_new_submissions_forwarding_url  = $config->get('default_on_new_submissions_forwarding_url');
+    if (is_string($default_on_new_submissions_forwarding_url)) { 
+      $default_on_new_submissions_forwarding_url  =  $default_on_new_submissions_forwarding_url ;
+    } else { 
+      $default_on_new_submissions_forwarding_url  = 'Enter fully qualified url here: https://example.com/post';
     }
 
     
@@ -85,45 +99,53 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
       '#title' => t('Advanced Webform Cookies Settings'),
       '#description' => t('Site wide webform cookie settings'),
     );
-    
-    $form['webform_cookies_handler_admin']['apply_to_all_webforms'] = array(
-      '#type' => 'checkbox',
+
+    $form['webform_cookies_handler_admin']['default_cookies_fieldset']  = array(
+      '#type' => 'fieldset',
       '#title' => t('Apply Webform Cookies Handler to All Webforms.'),
       '#description' => t('This setting will retroactively apply the handler to all webforms.
       However, it will not overwrite Webforms already configured with Webform Cookies Handler.'),
+    );
+    
+    
+    $form['webform_cookies_handler_admin']['default_cookies_fieldset']['apply_to_all_webforms'] = array(
+      '#type' => 'checkbox',
       '#default_value' => $default_all_webforms,
       '#required' => FALSE,
     );
 
-    $form['webform_cookies_handler_admin']['default_cookies'] = array(
+    $form['webform_cookies_handler_admin']['default_cookies_fieldset']['default_cookies'] = array(
       '#type' => 'textfield', 
-      '#title' => t('Default Cookies to be added to webforms'), 
-      '#description' => t('Apply Webforms Cookies Handler to all Webforms retroactively with these cookies set as default.'),
+      '#title' => t('Default Cookies'), 
+      //'#description' => t('Apply Webforms Cookies Handler to all Webforms retroactively with these cookies set as default.'),
       '#default_value' => $default_cookies,
       '#size' => 60, 
       '#maxlength' => 128, 
       '#required' => FALSE,
     );
 
-    $form['webform_cookies_handler_admin']['default_on_new_submissions'] = array(
-      '#type' => 'checkbox',
+    $form['webform_cookies_handler_admin']['default_on_new_submissions_fieldset']  = array(
+      '#type' => 'fieldset',
       '#title' => t('Apply Default Webform Cookies Handler for all new Webforms.'),
-      '#description' => t('This setting will pro-actively apply the handler to all future webforms.
-      However, these webforms can always be reconfigured and customized for the future.'),
+      '#description' => t('This setting will pro-actively apply the handler to all future webforms. However, these webforms can always be reconfigured and customized for the future.'),
+    );
+
+    $form['webform_cookies_handler_admin']['default_on_new_submissions_fieldset']['default_on_new_submissions'] = array(
+      '#type' => 'checkbox',  
       '#default_value' => $default_on_new_submissions,
       '#required' => FALSE,
     );
 
-    $form['webform_cookies_handler_admin']['default_new_submissions_cookies'] = array(
+    $form['webform_cookies_handler_admin']['default_on_new_submissions_fieldset']['default_new_submissions_cookies'] = array(
       '#type' => 'textfield', 
-      '#title' => t('Default Cookies to be added to newly created webforms'), 
-      '#description' => t('Apply Default Webform Cookies for all new Webforms.'),
+      '#title' => t('Default Cookies'), 
       '#default_value' => $default_new_submissions_cookies,
       '#size' => 60, 
       '#maxlength' => 128, 
       '#required' => FALSE,
       
     );
+
 
     # Additional settings for adding default automatic forwarding to all webforms
     $form['webform_cookies_handler_additional_admin_settings'] = array(
@@ -132,23 +154,46 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
       '#description' => t('Settings for applying URL forwarding to webforns'),
     );
 
-    $form['webform_cookies_handler_additional_admin_settings']['default_url_checkbox'] = array(
-      '#type' => 'checkbox',
+    $form['webform_cookies_handler_additional_admin_settings']['default_url_checkbox_fieldset']  = array(
+      '#type' => 'fieldset',
       '#title' => t('Retroactively Apply Default URL forwarding for all webforms'),
-      '#description' => t('retroactively apply default URL forwarding for all webforms. Will not overwrite webforms already configured with the URL forwarding handler.'),
+      '#description' => t('Retroactively apply default URL forwarding for all webforms. Will not overwrite webforms already configured with the URL forwarding handler.'),
+    );
+
+    $form['webform_cookies_handler_additional_admin_settings']['default_url_checkbox_fieldset']['default_url_checkbox'] = array(
+      '#type' => 'checkbox',
       '#default_value' => $default_url_checkbox,
       '#required' => FALSE,
     );
 
-    $form['webform_cookies_handler_additional_admin_settings']['default_forwarding_url'] = array(
+    $form['webform_cookies_handler_additional_admin_settings']['default_url_checkbox_fieldset']['default_forwarding_url'] = array(
       '#type' => 'textfield', 
-      '#title' => t('Default Fowarding URL'), 
-      '#description' => t('The URL to be retroactively applied for all webforms.'),
+      '#title' => t('Fowarding URL'), 
       '#default_value' => $default_forwarding_url,
       '#size' => 60, 
       '#maxlength' => 128, 
       '#required' => FALSE,
-      
+    );
+
+    $form['webform_cookies_handler_additional_admin_settings']['default_on_new_submissions_forwarding_checkbox_fieldset']  = array(
+      '#type' => 'fieldset',
+      '#title' => t('Add default forwarding for all new Webforms created.'),
+      '#description' => t('Apply default URL forwarding for all newly created webforms. However, these webforms can always be reconfigured and customized for the future.'),
+    );
+
+    $form['webform_cookies_handler_additional_admin_settings']['default_on_new_submissions_forwarding_checkbox_fieldset']['default_on_new_submissions_forwarding_checkbox'] = array(
+      '#type' => 'checkbox',
+      '#default_value' => $default_on_new_submissions_forwarding_checkbox,
+      '#required' => FALSE,
+    );
+
+    $form['webform_cookies_handler_additional_admin_settings']['default_on_new_submissions_forwarding_checkbox_fieldset']['default_on_new_submissions_forwarding_url'] = array(
+      '#type' => 'textfield', 
+      '#title' => t('Forwarding URL'), 
+      '#default_value' => $default_on_new_submissions_forwarding_url,
+      '#size' => 60, 
+      '#maxlength' => 128, 
+      '#required' => FALSE,
     );
     
     return parent::buildForm($form, $form_state);
@@ -183,11 +228,11 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
 
     # Set URL forwarding and forwarding URL 
     $this->config('webform_cookies_handler.settings')
-    ->set('default_url_checkbox', $values['default_url_checkbox'])
+    ->set('default_on_new_submissions_forwarding_checkbox', $values['default_on_new_submissions_forwarding_checkbox'])
     ->save();
 
     $this->config('webform_cookies_handler.settings')
-    ->set('default_forwarding_url', $values['default_forwarding_url'])
+    ->set('default_on_new_submissions_forwarding_url', $values['default_on_new_submissions_forwarding_url'])
     ->save();
    
 
@@ -244,11 +289,11 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
 
         $handlers = $webform->getHandlers();
         $already_there = FALSE;
-
+        // TODO: ADD ability to overwrite already configured cookies settings
         # Check if the handler already exists in this webform
         foreach ($handlers as $handle) {
           if ($handle instanceof CookiesWebformHandler) {
-            $webform->updateWebformHandler($handler);
+            # $webform->updateWebformHandler($handler);
             $already_there = TRUE;
             break;
           }
@@ -303,7 +348,7 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
     # Now going to open each webform one-by-one and add cookies handler with default config
     foreach ($ids as $index => $id) {
       $webform = \Drupal::entityTypeManager()->getStorage('webform')->load($id);
-      //ksm($webform->status());
+
       if ($webform) {
     
         // Must set original id so that the webform can be resaved.
@@ -312,11 +357,11 @@ class WebformCookiesHandlerConfigurationForm extends ConfigFormBase {
 
         $handlers = $webform->getHandlers();
         $already_there = FALSE;
-
+        // TODO: ADD ability to overwrite already configured URL settings
         # Check if the handler already exists in this webform
         foreach ($handlers as $handle) {
           if ($handle instanceof RemotePostWebformHandler) {
-            $webform->updateWebformHandler($handler);
+            # $webform->updateWebformHandler($handler);
             $already_there = TRUE;
             break;
           }
